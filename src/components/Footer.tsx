e40f127ee31092b069c3bd609023d0756b7676cb
iamsuman <suman.191544@ncit.edu.np>
2023-12-09 21:04:33 +0545

    Setup Complete for Shadcn-ui landing page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+
+const Footer = () => {
+  return (
+    <div>
+      Footer  <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default Footer
