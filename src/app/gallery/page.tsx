a8a4080eb39e2e6b66f2ee95a19c7432c9292ff8
iamsuman <suman.191544@ncit.edu.np>
2023-12-12 22:31:32 +0545

    card update

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import img from "@/../public/log.jpg";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Image from "next/image";
+
+export default function CardWithForm() {
+  return (
+    <Card className="w-[350px] m-10 text-center">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Image
+          src={img}
+          alt="hero"
+          width={300}
+          height="300"
+          className="hover:bg-contain"
+        />
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+  );
+}
