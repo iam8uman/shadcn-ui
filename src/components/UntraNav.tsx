2846e96ebb7396363949562af71a51114ef9577c
iamsuman <suman.191544@ncit.edu.np>
2023-12-13 14:39:46 +0545

    deployment friendly

--- src/components/UntraNav.tsx
+++ src/components/UntraNav.tsx
@@ -71,7 +71,7 @@ const UltraNav = () => {
       <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem>
-            <NavigationMenuTrigger className>Getting started</NavigationMenuTrigger>
+            <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
             <NavigationMenuContent>
               <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                 <li className="row-span-3">
